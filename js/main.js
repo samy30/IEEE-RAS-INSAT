4f16d02c53731f12876401a286655d753b742799
samy30 <samibelaid30@gmail.com>
2019-06-15 16:00:03 +0100

    add competitions

--- js/main.js
+++ js/main.js
@@ -128,11 +128,11 @@
 
   $(window).on('scroll', function () {
     var cur_pos = $(this).scrollTop();
-  
+
     nav_sections.each(function() {
       var top = $(this).offset().top - main_nav_height,
           bottom = top + $(this).outerHeight();
-  
+
       if (cur_pos >= top && cur_pos <= bottom) {
         main_nav.find('li').removeClass('menu-active menu-item-active');
         main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('menu-active menu-item-active');
